c469cb09d8bef9031ea7e73fe96af734c6d0d170
Eric Stokes <eestokes@pm.me>
2026-05-31 10:32:40 -0400

    add comment about how to run it on windows

--- examples/auth.rs
+++ examples/auth.rs
@@ -1,6 +1,17 @@
 use bytes::Bytes;
-use cross_krb5::{AcceptFlags, ClientCtx, InitiateFlags, K5Ctx, Step, ServerCtx};
+use cross_krb5::{AcceptFlags, ClientCtx, InitiateFlags, K5Ctx, ServerCtx, Step};
 use std::{env::args, process::exit, sync::mpsc, thread};
+
+// The auth example pushes the server and the client in one process, which SSPI
+// has issues with because keytabs are not a thing in windows. You can work around
+// this by running it as SYSTEM and targeting the machine account, assuming you are
+// joined to a domain.
+//
+// ````
+// schtasks /create /tn krbtest /ru SYSTEM /sc once /st 23:59 /f /tr "cmd /c cd /d C:\Users\eric\proj\cross-krb5 && .\target\debug\examples\auth.exe HOST/win11-test-vm.ryu-oh.org > C:\krbtest.txt 2>&1"
+// schtasks /run /tn krbtest & timeout 2 & type C:\krbtest.txt
+// schtasks /delete /tn krbtest /f
+// ````
 
 enum Msg {
     Token(Bytes),
@@ -17,10 +28,12 @@ fn server(spn: String, input: mpsc::Receiver<Msg>, output: mpsc::Sender<Msg>) {
         match server.step(&*token).expect("step") {
             Step::Finished((ctx, token)) => {
                 if let Some(token) = token {
-                    output.send(Msg::Token(Bytes::copy_from_slice(&*token))).expect("send");
+                    output
+                        .send(Msg::Token(Bytes::copy_from_slice(&*token)))
+                        .expect("send");
                 }
-                break ctx
-            },
+                break ctx;
+            }
             Step::Continue((ctx, token)) => {
                 output.send(Msg::Token(Bytes::copy_from_slice(&*token))).expect("send");
                 server = ctx;
@@ -48,10 +61,12 @@ fn client(spn: &str, input: mpsc::Receiver<Msg>, output: mpsc::Sender<Msg>) {
         match client.step(&*token).expect("step") {
             Step::Finished((ctx, token)) => {
                 if let Some(token) = token {
-                    output.send(Msg::Token(Bytes::copy_from_slice(&*token))).expect("send");
+                    output
+                        .send(Msg::Token(Bytes::copy_from_slice(&*token)))
+                        .expect("send");
                 }
-                break ctx
-            },
+                break ctx;
+            }
             Step::Continue((ctx, token)) => {
                 output.send(Msg::Token(Bytes::copy_from_slice(&*token))).expect("send");
                 client = ctx;
